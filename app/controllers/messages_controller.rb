5d003f38f9d986492abeaa405ffb5d943993aa40
nao-gabu0445 <78404803+nao-gabu0445@users.noreply.github.com>
2021-02-10 18:05:36 +0900

    チャットルームの削除機能の実装(ビューの修正跡)

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -6,7 +6,7 @@ class MessagesController < ApplicationController
   end
   def create
     @room=Room.find(params[:room_id])
-    @message = @room.mesages.new(message_params)
+    @message = @room.messages.new(message_params)
     if @message.save
       redirect_to room_messages_path(@room)
       else
