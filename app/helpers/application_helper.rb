62c5133b9ff51d4a2ebea2c7a56a5c9ed72513ca
nao-gabu0445 <78404803+nao-gabu0445@users.noreply.github.com>
2021-03-17 11:19:49 +0900

    URL実験

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  require "uri"
+
+  def text_url_to_link(text)
+
+    URI.extract(text,["http","https"]).uniq.each do |url|
+      text.gsub!(url,"#{url}")
+    end
+    text
+  end
+  
 end
